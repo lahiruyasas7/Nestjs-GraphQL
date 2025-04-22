432d6a61a60c1ab7094775a40d18c1d169457804
Lahiru Yasas <lahiruyasas7@gmail.com>
2025-04-22 15:59:16 +0530

    added typeorm and db config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import dbConfig from './config/db.config';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+      load: [dbConfig],
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      useFactory: dbConfig,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
