648d13c834b9dba3eba0788f7ba1160d5275079d
Lahiru Yasas <lahiruyasas7@gmail.com>
2025-06-06 12:25:07 +0530

    feat: added userservice

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -6,6 +6,7 @@ import { User } from 'src/entities/user.entity';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User])],
-  providers: [UserResolver, UserService]
+  providers: [UserResolver, UserService],
+  exports: [UserService],
 })
 export class UserModule {}
