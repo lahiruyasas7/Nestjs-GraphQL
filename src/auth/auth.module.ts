2e3a41b9d3ac1a913a3e94568eb203abf08134c3
Lahiru Yasas <lahiruyasas7@gmail.com>
2025-06-06 13:58:31 +0530

    feat: crated auth module

---
+++ src/auth/auth.module.ts
@@ -0,0 +1,21 @@
+import { Module } from '@nestjs/common';
+import { JwtModule, JwtService } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
+import { AuthService } from './auth.service';
+import { UserModule } from 'src/user/user.module';
+import { AuthResolver } from './auth.resolver';
+import { JwtStrategy } from './jwt.strategy';
+import { UserService } from 'src/user/user.service';
+
+@Module({
+  imports: [
+    PassportModule,
+    JwtModule.register({
+      secret: process.env.JWT_SECRET,
+      signOptions: { expiresIn: '1d' },
+    }),
+    UserModule,
+  ],
+  providers: [AuthService, AuthResolver, JwtStrategy, JwtService],
+})
+export class AuthModule {}
